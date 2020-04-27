690e80b58b5b5b51491ff8e1f5a9db373a3c1b79
moy5 <55164803+moy5@users.noreply.github.com>
2020-04-27 15:16:09 -0500

    Environment added, some puzzle functionality for first room

---
+++ Assets/KeyControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyControl : MonoBehaviour
+
+{
+    public AudioClip sound;
+    public AudioSource audioSource;
+    private Animator anim;
+    public string whichAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsTag("0"))
+        {
+            audioSource.PlayOneShot(sound);
+            anim.Play(whichAnimation);
+        }  
+    }
+}
---
+++ Assets/MusicBoxSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBoxSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+    public AudioSource audioSource;
+    public AudioClip sound;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsTag("0"))
+        {
+            audioSource.PlayOneShot(sound);
+            anim.Play("Rotate");
+        }
+    }
+}
