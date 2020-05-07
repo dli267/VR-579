4f645de23678c0fd6d7aaaed92123bd1466d7098
dli267 <dli267@wisc.edu>
2020-05-06 20:30:56 -0700

    Add sunroom

---
+++ Assets/moveThing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveThing : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectToMove = null;
+    [SerializeField]
+    float move = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        objectToMove.transform.position += new Vector3(0, 0, move); 
+    }
+}
